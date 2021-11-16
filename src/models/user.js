135bdc248df2a49941f26ac62f7231cb599be6e6
ImAkhil24 <akhilbadoni24@gmail.com>
2021-11-16 23:44:57 +0530

    added authentication to task routes

--- src/models/user.js
+++ src/models/user.js
@@ -11,7 +11,7 @@ const userSchema = new mongoose.Schema({
   },
   email: {
     type: String,
-    unique: true, 
+    unique: true,
     required: true,
     trim: true,
     lowercase: true,
@@ -38,21 +38,29 @@ const userSchema = new mongoose.Schema({
       }
     },
   },
-  tokens: [{
-    token: {
-      type: String,
-      required: true,
-    }
-  }]
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+userSchema.virtual("tasks", {
+  ref: "Task",
+  localField: '_id',
+  foreignField: 'owner'
 });
 
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
-  const token = jwt.sign({ _id: user.id.toString() }, 'akhil');
+  const token = jwt.sign({ _id: user.id.toString() }, "akhil");
   user.tokens = user.tokens.concat({ token });
   await user.save();
   return token;
-}
+};
 
 userSchema.methods.toJSON = function () {
   const user = this;
@@ -62,23 +70,23 @@ userSchema.methods.toJSON = function () {
   delete userObject.tokens;
 
   return userObject;
-}
+};
 
 userSchema.statics.findByCredentials = async (email, password) => {
   const user = await User.findOne({ email });
 
-  if(!user) {
-    throw new Error('Unable to login');
+  if (!user) {
+    throw new Error("Unable to login");
   }
 
   const isMatch = await bcrypt.compare(password, user.password);
 
-  if(!isMatch) {
-    throw new Error('Unable to login');
+  if (!isMatch) {
+    throw new Error("Unable to login");
   }
 
   return user;
-}
+};
 
 // Hash the plain text password before saving
 userSchema.pre("save", async function (next) {
